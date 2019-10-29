6194f8a5d6b4cd8213d02f39f7ccb30e91dad688
Aimee MacDonald <aimeelmacdonald@gmail.com>
2019-10-29 15:29:59 +0200

    dotenv

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 
 app.get("/", (req, res) => {
   res.status(200).send("2BSure");
 });
 
-app.listen(8080, () => {console.log("Server started")});
+app.listen(process.env.PORT, () => {console.log("Server started")});
