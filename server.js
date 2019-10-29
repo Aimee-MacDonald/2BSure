9c89fbef1929991c67891b4546b21bbc78084b19
Aimee MacDonald <aimeelmacdonald@gmail.com>
2019-10-29 17:05:13 +0200

    Login and Register Pages

--- server.js
+++ server.js
@@ -12,4 +12,12 @@ app.get("/", (req, res) => {
   res.status(200).render("holding");
 });
 
+app.get("/login", (req, res) => {
+  res.status(200).render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.status(200).render("register");
+});
+
 app.listen(process.env.PORT, () => {console.log("Server started")});
