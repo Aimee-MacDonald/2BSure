b9580627ef08b2c859239b5b02b938764f8265f6
Aimee MacDonald <aimeelmacdonald@gmail.com>
2019-10-29 15:37:10 +0200

    Pug Templating Engine

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const path = require("path");
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
-  res.status(200).send("2BSure");
+  res.status(200).render("holding");
 });
 
 app.listen(process.env.PORT, () => {console.log("Server started")});
