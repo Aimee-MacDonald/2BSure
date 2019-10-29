f37696c1661422ad8952c9b2647759017aecee99
Aimee MacDonald <aimeelmacdonald@gmail.com>
2019-10-29 16:02:10 +0200

    Holding page

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const path = require("path");
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "/views"));
 
+app.use(express.static(path.join(__dirname, "/static")));
+
 app.get("/", (req, res) => {
   res.status(200).render("holding");
 });
