241622695bbe06ca4680f55a3caa08d9a45e3f09
Aimee MacDonald <aimeelmacdonald@gmail.com>
2020-01-30 19:47:36 +0200

    Added Total Cost to Cart Schema

--- dbmodels/cart.js
+++ dbmodels/cart.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 var schema = new Schema({
   userID: {type: String, required: true},
   lastEdit: {type: Date, required: true},
-  products: {type: Array, required: false}
+  products: {type: Array, required: false},
+  total: {type: Number, required: true}
 });
 
 module.exports = mongoose.model("cart", schema);
