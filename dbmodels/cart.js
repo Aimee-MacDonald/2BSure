2bf3263eab913f3099934172dfe031d11b20ce1f
Aimee MacDonald <aimeelmacdonald@gmail.com>
2020-01-30 17:58:02 +0200

    Update Cart Schema

--- dbmodels/cart.js
+++ dbmodels/cart.js
@@ -4,9 +4,7 @@ const Schema = mongoose.Schema;
 var schema = new Schema({
   userID: {type: String, required: true},
   lastEdit: {type: Date, required: true},
-  product1: {type: Number, required: false},
-  product2: {type: Number, required: false},
-  product3: {type: Number, required: false}
+  products: {type: Array, required: false}
 });
 
 module.exports = mongoose.model("cart", schema);
