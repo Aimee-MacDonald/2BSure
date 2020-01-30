f4b0a617c411873f020c418ea711f896fa85f32b
Aimee MacDonald <aimeelmacdonald@gmail.com>
2020-01-30 09:22:59 +0200

    Extended Product DB Model

--- dbmodels/product.js
+++ dbmodels/product.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 var schema = new Schema({
-  name: {type: String, required: true}
+  name: {type: String, required: true},
+  price: {type: Number, required: true},
+  description: {type: String, required: true}
 });
 
 module.exports = mongoose.model("product", schema);
