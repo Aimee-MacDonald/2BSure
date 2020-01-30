0a75e1fe13cf2ed992be7808c811a92f1026e617
Aimee MacDonald <aimeelmacdonald@gmail.com>
2020-01-30 09:52:38 +0200

    Added Image URL to Product Schema

--- dbmodels/product.js
+++ dbmodels/product.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 var schema = new Schema({
+  imageURL: {type: String, required: true},
   name: {type: String, required: true},
   price: {type: Number, required: true},
   description: {type: String, required: true}
