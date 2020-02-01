0d15a3e35b95255630e3963ce97989bb3136b2f6
Aimee MacDonald <aimeelmacdonald@gmail.com>
2020-02-01 22:35:02 +0200

    Refactored Products in Order Schema

--- dbmodels/order.js
+++ dbmodels/order.js
@@ -4,9 +4,7 @@ const Schema = mongoose.Schema;
 var schema = new Schema({
   userID: {type: String, required: true},
   status: {type: String, required: true},
-  product1: {type: Number, required: false},
-  product2: {type: Number, required: false},
-  product3: {type: Number, required: false}
+  products: {type: Array, required: true}
 });
 
 module.exports = mongoose.model("order", schema);
