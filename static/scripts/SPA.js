ae5f5a9a82e3a32b27909133d03a5dd10c9e90a3
Aimee MacDonald <aimeelmacdonald@gmail.com>
2020-02-09 12:06:46 +0200

    SPA Landing Page

--- static/scripts/SPA.js
+++ static/scripts/SPA.js
@@ -4,6 +4,7 @@ resetSPA();
 function resetSPA(){
   clearBody();
   addStyle();
+  addElements();
 }
 
 // Remove all Elements from Body
@@ -23,4 +24,29 @@ function addStyle(){
   el_style.setAttribute("rel", "stylesheet");
   el_style.setAttribute("href", "/styles/spa.css");
   el_head.append(el_style);
+}
+
+function addElements(){
+  var el_body = document.getElementById("body");
+
+  var el_splash = document.createElement("img");
+  el_splash.setAttribute("src", "/images/ketomojo.svg");
+  el_splash.setAttribute("alt", "2BSure");
+  el_splash.setAttribute("id", "splash");
+
+  var el_p = document.createElement("p");
+  el_p.innerText = "Short descriptive paragraph explaining the purpose and importance of ketomojo";
+
+  var el_info_link = document.createElement("a");
+  el_info_link.setAttribute("href", "#info");
+  el_info_link.innerText = "Learn More";
+
+  var el_products_link = document.createElement("a");
+  el_products_link.setAttribute("href", "#products");
+  el_products_link.innerText = "Order Now";
+
+  el_body.append(el_splash);
+  el_body.append(el_p);
+  el_body.append(el_info_link);
+  el_body.append(el_products_link);
 }
