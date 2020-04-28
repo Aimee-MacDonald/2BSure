1799506ded6a18dff73c30e4ea7ddedb30ab584b
Aimee MacDonald <aimeelmacdonald@gmail.com>
2020-04-28 14:28:12 +0200

    Moved Address Verification to Payment Page

--- routes/payment.js
+++ routes/payment.js
@@ -16,79 +16,10 @@ router.get("/", (req, res) => {
           var respac = {};
           respac.total = crt.total;
           respac.cartID = crt.id;
+          respac.csrfToken = req.csrfToken();
           res.status(200).render("payment", respac);
         } else {
           res.redirect("/cart");
-        }
-      }
-    });
-  } else {
-    res.redirect("/login");
-  }
-});
-
-router.get("/verifyAddress", (req, res) => {
-  if(req.isAuthenticated()){
-    Address.findOne({'userID': req.session.passport.user}, (err, adr) => {
-      if(err){
-        res.redirect("/error");
-      } else {
-        var respac = {};
-        respac.csrfToken = req.csrfToken();
-
-        if(adr){
-          respac.line1 = adr.line1;
-          respac.line2 = adr.line2;
-          respac.town = adr.town;
-          respac.province = adr.province;
-          respac.postcode = adr.postcode;
-        }
-
-        res.status(200).render("verifyAddress", respac);
-      }
-    });
-  } else {
-     res.redirect("/login");
-  }
-});
-
-router.post("/verifyAddress", (req, res) => {
-  if(req.isAuthenticated()){
-    Address.findOne({'userID': req.session.passport.user}, (err, adr) => {
-      if(err){
-        res.redirect("/error");
-      } else {
-        if(adr){
-          adr.line1 = req.body.line1;
-          adr.line2 = req.body.line2;
-          adr.town = req.body.town;
-          adr.province = req.body.province;
-          adr.postcode = req.body.postcode;
-
-          adr.save(err2 => {
-            if(err2){
-              res.redirect("/error");
-            } else {
-              res.redirect("/payment");
-            }
-          });
-        } else {
-          var newAdr = new Address({
-            'userID': req.session.passport.user,
-            'line1': req.body.line1,
-            'line2': req.body.line2,
-            'town': req.body.town,
-            'province': req.body.province,
-            'postcode': req.body.postcode
-          });
-
-          newAdr.save(err3 => {
-            if(err3){
-              res.redirect("/error");
-            } else {
-              res.redirect("/payment");
-            }
-          });
         }
       }
     });
